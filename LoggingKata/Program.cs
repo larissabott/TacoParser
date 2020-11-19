9b0bc65d447bea4d06757d63ec0e6765f25c62ae
[larissa bottefield] <[larissabott3@gmail.com]>
2020-11-19 09:21:53 -0600

    toco bell update

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -10,6 +10,8 @@ namespace LoggingKata
         static readonly ILog logger = new TacoLogger();
         const string csvPath = "TacoBell-US-AL.csv";
 
+        public static GeoCoordinate corB { get; private set; }
+
         static void Main(string[] args)
         {
             // TODO:  Find the two Taco Bells that are the furthest from one another.
@@ -35,18 +37,42 @@ namespace LoggingKata
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
             // Create a `double` variable to store the distance
+            ITrackable location1 = null;
+            ITrackable location2 = null;
+            double distance = 0;
 
             // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
             //HINT NESTED LOOPS SECTION---------------------
             // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
+            for (int i =0; i < locations.Length; i++)
+            {
+                var loc1 = locations[i];
+                var corA = new GeoCoordinate();
+                corA.Latitude = locations[i].Location.Latitude;
+                corA.Longitude = locations[i].Location.Longitude;
 
-            // Create a new corA Coordinate with your locA's lat and long
+                // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
+                for (int d= 0; d< location2.Length; d++)
+                {
+                    var loc2 = locations[d];
+                    // Create a new Coordinate with your locB's lat and long
+                    var corB = new GeoCoordinate();
+                    corB.Latitude = locations[d].Location.Latitude;
+                    corB.Longitude = locations[d].Location.Longitude;
 
-            // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
+                    if (corA.GetDistanceTo(corB) > distance)
+                    {
+                        distance = corA.GetDistanceTo(corB);
+                        location1 = locations[i];
+                        location2 = locations[d];
+                    }
 
-            // Create a new Coordinate with your locB's lat and long
-
+                }
+                
+            }
+            Console.WriteLine($"{location1.Name} {location2.Name}");
+            Console.WriteLine($"{distance * .000621371192}");
             // Now, compare the two using `.GetDistanceTo()`, which returns a double
             // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
 
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -23,8 +23,11 @@
             }
 
             // grab the latitude from your array at index 0
+            var latitude = double.Parse(cells[0]);
             // grab the longitude from your array at index 1
+            var longitude = double.Parse(cells[1]);
             // grab the name from your array at index 2
+            var name = cells[2];
 
             // Your going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
